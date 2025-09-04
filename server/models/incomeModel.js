461e4354ce5e7f64b747912949ab502893a39df9
Ketan Maheshwari <ketanmaheshwari1502@gmail.com>
2025-09-04 16:51:19 +0530

    adding income and user models

---
+++ server/models/incomeModel.js
@@ -0,0 +1,20 @@
+const {mongoose} = require('mongoose');
+
+const incomeSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    title:{
+        type: String,
+        required: true
+    },
+    amount:{
+        type: Number,
+        required: true
+    }
+},
+{timestamps: true}
+)
+
+module.exports = mongoose.model('Income' , incomeSchema)
